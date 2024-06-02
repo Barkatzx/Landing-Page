db67a3c28ed2ade49ab2b47bd8e262a9ac4be40e
Barkatzx <barkatullah.zx@gmail.com>
2024-06-03 03:50:07 +0600

    Update 2.0

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ window.onscroll = function() {
         navbar.classList.remove('active');
     }
 };
+
+// This function will run when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+  // Select the loading page element
+  var loader = document.getElementById("loading-page");
+  // Hide the loading page
+  loader.style.display = "none";
+});
+
+
+
+
+
